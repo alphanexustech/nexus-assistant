ec39aadf2e575f7ed37a2e141ebac807d375d244
Nathaniel Buechler <natebuechler@gmail.com>
2018-01-03 16:35:50 -0500

    chore: Add config files

---
+++ config/microservices.js
@@ -0,0 +1,12 @@
+/**
+ * IP Addresses for microservices
+ */
+
+module.exports = {
+
+  gatewayServer: '0.0.0.0',
+  gatewayPort: '9000',
+  interceptorServer: '0.0.0.0',
+  interceptorPort: '5000'
+
+}
