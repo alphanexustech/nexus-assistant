4016788028add7a482382f62868d4a82231599b2
Nathaniel Buechler <natebuechler@gmail.com>
2018-04-04 21:16:12 -0400

    feat: Integration of role-scorer components

--- routes/scorer.js
+++ routes/scorer.js
@@ -40,6 +40,30 @@ router.post('/analyze_emotion_set', passportConf.isAuthenticated, function(req, 
 });
 
 /**
+ * POST /
+ * N-Interceptor method to ask N-Scorer to score based on the body.
+ */
+router.post('/analyze_role_set', passportConf.isAuthenticated, function(req, res, next) {
+  var token = req.headers.authorization;
+  var userID = authentication.checkToken(token)[1]['sub']
+  var username = authentication.checkToken(token)[1]['username']
+  var payload = req.body // Pass the body as a new payload for a new request
+  // But, add to the payload information from the JWT token.
+  payload['id'] = userID,
+  payload['username'] = username,
+  axios.post(interceptorURL + '/scorer/analyze_role_set/', payload)
+  .then(function (response) {
+    return res.send({
+      "data": response.data
+    })
+  })
+  .catch(function (error) {
+    console.log(error);
+    return res.send(error.data)
+  });
+});
+
+/**
  * GET /
  * N-Interceptor method to retrieve all prior run analyses for a user based on the token.
  * Specifies params:
@@ -101,5 +125,79 @@ router.get('/analyses/:collection/:id', passportConf.isAuthenticated, function(r
     return res.send(error.data)
   });
 })
+
+
+/**
+ *
+ *** More flexible methods for the future that support more than affect.
+ *
+ */
+
+
+/**
+ * GET /
+ * N-Interceptor method to retrieve all prior run analyses for a user based on the token.
+ * Specifies params:
+    collection (string)
+    page (number)
+    countPerPage (number)
+ */
+router.get('/analyses/:database/:collection/:page/:countPerPage', passportConf.isAuthenticated, function(req, res, next) {
+  var token = req.headers.authorization;
+  var database = req.params.database,
+      collection = req.params.collection,
+      page = req.params.page,
+      countPerPage = req.params.countPerPage;
+
+  var config = {
+    headers: {
+      "Authorization": token // Set authorization header
+    }
+  }
+  axios.get(
+    interceptorURL + '/scorer/analyses/' + database + '/' + collection + '/' + page  + '/' + countPerPage,
+    config)
+  .then(function (response) {
+    return res.send({
+      "data": response.data
+    })
+  })
+  .catch(function (error) {
+    console.log(error);
+    return res.send(error.data)
+  });
+});
+
+/**
+ * GET /
+ * N-Interceptor method to retrieve single prior run analysis for a user based on the token.
+    collection (string)
+    id (string-ed hash)
+ */
+router.get('/analyses/:database/:collection/:id', passportConf.isAuthenticated, function(req, res, next) {
+  var token = req.headers.authorization;
+  var database = req.params.database,
+      collection = req.params.collection,
+      id = req.params.id;
+
+  var config = {
+    headers: {
+      "Authorization": token // Set authorization header
+    }
+  }
+  axios.get(
+    interceptorURL + '/scorer/analyses/' + database + '/' + collection + '/' + id,
+    config)
+  .then(function (response) {
+    return res.send({
+      "data": response.data
+    })
+  })
+  .catch(function (error) {
+    console.log(error);
+    return res.send(error.data)
+  });
+})
+
 
 module.exports = router;
